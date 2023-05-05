1d8a902220fa6166e9522228c0fd2aae4262fe0e
alankosofia <132684241+alankosofia@users.noreply.github.com>
2023-05-05 19:48:17 +0300

    header

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,11 +7,7 @@ const Home = () => {
 		<>
 			<Header />
 			<Link to="/chat">
-<<<<<<< Updated upstream
 				<p className="text-base lg:text-2xl text-sad-purple">Moi!</p>;
-=======
-				<p className="text-base lg:text-2xl bg-purple-700">Moi!</p>
->>>>>>> Stashed changes
 			</Link>
 		</>
 	);
