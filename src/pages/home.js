6172bb632f936c4ccf82bacfb1cf10ea814e753c
viivi <vihne.viivi@gmail.com>
2023-05-05 19:23:54 +0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/header";
+import Man from "../assets/photos/man.png";
 
 const Home = () => {
 	return (
@@ -9,6 +10,11 @@ const Home = () => {
 			<Link to="/chat">
 				<p className="text-base lg:text-2xl bg-purple-700">Moi!</p>;
 			</Link>
+			<div className = "flex justify-between">
+				<p className = "object-right" ></p> 
+				<img className="w-1/2" src = {Man} alt = "man" />;
+			</div>
+			
 		</>
 	);
 };
