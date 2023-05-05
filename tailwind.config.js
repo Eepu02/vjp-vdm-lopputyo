a6fe70c5f03c3cc74a9c26bc64221d0e47512aa4
Eepu02 <eemil.lahteenmaki@gmail.com>
2023-05-05 19:32:47 +0300

    Lisätty värit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
+		colors: {
+			"sad-purple": "#7E6E8A",
+			"happy-yellow": "#FCEA2B",
+		},
 	},
 	plugins: [],
 };
