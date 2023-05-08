5552313a29de0a937e29e01b903277ea33c5e35e
alankosofia <132684241+alankosofia@users.noreply.github.com>
2023-05-08 15:46:46 +0300

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Footer = () => {
+	return (
+		<div className="bg-candyfloss-purple flex justify-around pt-4 mt-10">
+		  <p className="text-dragon-purple text-base lg:text-xl">tunteetovatkaikille.fi</p>
+		</div>
+
+	); 
+};
+
+export default Footer;
