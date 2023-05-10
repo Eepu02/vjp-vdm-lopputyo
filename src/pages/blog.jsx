1ab134f04260b4ed922887c5bbe4e0755b916554
viivi <vihne.viivi@gmail.com>
2023-05-10 22:49:05 +0300

    blog.jsx muokkaus

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layouts/layout";
 import { useRef } from "react";
+import Cover from "../assets/photos/AdobeStock.jpeg";
 
 const Blog = () => {
     const blogi1 = useRef();
@@ -13,25 +14,35 @@ const Blog = () => {
         })
     }
 
-
-
         return (
         <Layout>
             <body>
                 <div className="bg-zinc-300">
-                    <div className="pt-60 flex flex-col justify-center items-center">
-                        <p className="text-dragon-purple font-josefin text-5xl">
-                            Itkevä Mies 
-                        </p>
-                        <p className="text-dragon-purple font-urbanist text-2xl">
-                            - Miehistä Miehisin
-                        </p>
-                        <p className="font-urbanist text-dragon-purple text-4xl pt-40">
+
+                    <div className=" flex flex-col justify-center items-center">
+                        <hr className="custom-line " />
+                        <img src={Cover} alt="blog-background" className="" />
+                        <hr className="custom-line my" />
+                        <div className="absolute top-0 left-20 w-full h-full flex flex-col justify-center items-center">
+                            <p className=" text-happy-yellow font-bold font-josefin text-5xl pd-4">
+                                Itkevä Mies 
+                            </p>
+                            <div className=" transform rounded-lg bg-gray-200 pr-2 pl-2">
+                                <p className="text-dragon-purple font-urbanist text-2xl">
+                                    - Miehistä Miehisin
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <p className="font-urbanist  text-4xl pt-40">
                             Viimeisimmät julkaisut
                         </p>
+                        <hr className="yellow-line my-12" />
+                    
+
                     </div>
 
-                    <div className="flex flex-col lg: m-12 grid grid-cols-3 gap-8 mb-52">
+                    <div className="flex flex-col lg: m-12 grid grid-cols-3 gap-8 mb-52 pt-24">
                         <div className=" flex tall-box transform rounded-lg  border-2 border-white shadow bg-gradient-to-r bg-sad-purple p-8 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 flex flex-col items-center justify-center"
                             onClick={() => scroll(blogi1)}>
                             <h1 className="text-2xl text-white font-josefin">Tunteiden ilmaisu ja ymmärtäminen voimavarana</h1>
