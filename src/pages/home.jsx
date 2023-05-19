2c8ea9572ac49787e07f9cf056edc77da1c5a227
viivi <vihne.viivi@gmail.com>
2023-05-19 18:47:22 +0300

    korjaus

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,13 +8,13 @@ import Video from "../components/home/video";
 
 const Home = () => {
     return (
-        <Layout className="overflow-x-hidden" >
+        <Layout className="" >
             <div className="flex flex-col justify-between md:flex-row lg:flex-row ">
                 <div className="flex-col pl-16 lg:pl-24 w-full lg:w-1/2 md:w-1/2 md:pl-16 md:h-full">
-                    <h1 className="pt-12 pl-4 pr-4 lg:pt-24 md:pt-24 lg:pl-10 font-urbanist text-4xl lg:text-6xl text-white vertical-line">
+                    <h1 className="pt-12 ml-4 mr-4 lg:pt-24 md:pt-24 lg:pl-10 font-urbanist text-4xl lg:text-6xl text-white vertical-line">
                         Tunteet ovat sokeita sukupuolelle
                     </h1>
-                    <p className="pt-8 md:pt-16 pl-4 pr-4 lg:pt-60 lg:pl-12 font-roboto text-base lg:text-lg text-white pr-8 ">
+                    <p className="pt-8 md:pt-16 pl-4 pr-4 lg:pt-60 lg:pl-12 font-roboto text-base lg:text-lg text-white mr-8 ">
                         <p className="">Miehisyyttä ja emotionaalisuutta pidetään herkästi toisiaan poissulkevina, 
                         eikä tunteista puhuminen tunnu aina turvalliselta ja sallitulta. Haluamme tarjota sinulle turvallisen tilan puhua tunteistasi ja mahdollisesti oppia tunnetyöstä enemmän. 
                         Kerro meille miltä sinusta tuntuu, kuten tunteet, mekin olemme sokeita sukupuolelle.
@@ -22,10 +22,10 @@ const Home = () => {
                     </p>
                 </div>
                 <div className="w-full lg:w-1/2 md:w-1/2 md:pr-0 md:pt-48">
-                    <img className="w-full h-auto" src={Man} alt="man" />
+                    <img className=" h-auto" src={Man} alt="man" />
                 </div>
             </div>
-            <button className="animate-bounce absolute bottom-0 right-16 lg:right-36 lg:mr-4  mt-4 lg:mt-10 transform rounded-2xl border-2 border-dragon-purple shadow bg-blue-600 bg-gradient-to-r from-happy-yellow via-yellow-300 border-yellow-300 to-yellow-400 p-6 lg:p-8 shadow-2xl transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-black">
+            <button className="animate-bounce absolute bottom-0 right-8 lg:right-36 lg:mr-4  mt-4 lg:mt-10 transform rounded-2xl border-2 border-dragon-purple shadow bg-blue-600 bg-gradient-to-r from-happy-yellow via-yellow-300 border-yellow-300 to-yellow-400 p-6 lg:p-8 shadow-2xl transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-black">
                 <Link to="/chat">
                     <p className=" font-roboto text-zinc-700 lg:text-2xl">
                         JUTELLAAN HETKI?
@@ -36,11 +36,11 @@ const Home = () => {
                 <div className="relative flex flex-col items-center w-full lg:w-1/2">
                     <div className="pt-16 md:pt-44 lg:pt-36">
                         <img src={Pie} alt="pie-chart" className=" h-auto transform scale-x-[-1] " />
-                        <div className="absolute top-24 md:top-28 right-8 md:right-4 lg:right-16 "style={{ width: "40%" }} >
-                            <div className="text-black lg:text-black font-josefin font-bold text-base lg:text-2xl px-4 md:px-2 lg:px-8"> 76% 18-34 vuotiaista miehistä tietää että tunteista puhuminen on hyväksi mielenterveydelle
+                        <div className="absolute top-16 md:top-28 right-4 md:right-4 lg:right-16 "style={{ width: "40%" }} >
+                            <div className="text-black lg:text-black font-josefin font-bold text-xs md:tet-base lg:text-2xl px-4 md:px-2 lg:px-8"> 76% 18-34 vuotiaista miehistä tietää että tunteista puhuminen on hyväksi mielenterveydelle
                         </div>    
                         </div>
-                        <button className="mt-8 lg:mt-12 px-16  lg:px-16 py-2 ml-40 lg:ml-52 flex items-center justify-center mt-4 lg:mt-10 transform rounded-full border-2 border-yellow-200 bg-gradient-to-r from-happy-yellow via-yellow-300 to-yellow-400 p-4 lg:p-8 transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-black">
+                        <button className="mt-8 lg:mt-12 px-16  lg:px-16 py-2 ml-20 lg:ml-52 flex items-center justify-center mt-4 lg:mt-10 transform rounded-full border-2 border-yellow-200 bg-gradient-to-r from-happy-yellow via-yellow-300 to-yellow-400 p-4 lg:p-8 transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-black">
                             <Link to="/blog">
                                 <p className="text-base font-roboto text-dragon-purple lg:text-2xl">
                                 LUE LISÄÄ!
@@ -84,8 +84,8 @@ const Home = () => {
                 </div>
                 <div className="relative flex flex-col items-center  pt-20 lg:pt-40 w-full lg:w-1/2 lg:ml-4 lg:ml-36">
                     <img src={Pilar} alt="pilar-graph" className="w-full h-auto" style={{ width: "65%" }} />
-                    <div className="absolute lg:top-20 md:top-28 top-24 lg:top-4 left-20 md:left-32 " style={{ width: "40%" }} >
-                        <p className=" font-josefin font-bold text-base text-black  lg:text-2xl md:text-xl px-4 md:px-8 lg:px-8">
+                    <div className="absolute lg:top-20 md:top-28 top-20 lg:top-4 left-20 md:left-32 " style={{ width: "40%" }} >
+                        <p className=" font-josefin font-bold  text-xs md:text-base text-black  lg:text-2xl md:text-xl px-4 md:px-8 lg:px-8">
                             Silti yli 58% miehistä kokee painetta olla emotionaalisesti vahva
                         </p>
                     </div>
@@ -99,7 +99,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="flex justify-between pt-20  ">
-                <div className="flex flex-col w-1/2 items-center pl-8 pr-8">
+                <div className="flex flex-col w-1/2 items-center pl-4 pr-4">
                     <p className="text-2xl lg:text-4xl text-white font-josefin pt-56">Me kuuntelemme </p>
                 </div>
                 <div className="flex flex-col justify-center">
